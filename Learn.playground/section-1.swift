aeec73420eb21ea51d87024df84f1896ef1d2fee
Ronaldo Santana <ronaldosantana@ronaldo.local>
2014-07-25 14:35:59 +1200

    control flow

--- Learn.playground/section-1.swift
+++ Learn.playground/section-1.swift
@@ -32,4 +32,15 @@ occupations["Jayne"] = "Public Relations"
 let emptyArray = [String]()
 let emptyDictionary = Dictionary<String, Float>()
 
-
+// control flow
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+// simple line with variable works on playground env to see var value
+teamScore
